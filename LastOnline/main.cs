62c4fab70847aa9922c3e20a79b5060d8c4cb621
Ancientgods <devoogdlaurens2@hotmail.com>
2015-07-06 13:45:31 +0200

    Updated for 1.3

--- LastOnline/main.cs
+++ LastOnline/main.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace Last_Online
 {
-    [ApiVersion(1, 17)]
+    [ApiVersion(1, 18)]
     public class LastOnline : TerrariaPlugin
     {
         public override Version Version
